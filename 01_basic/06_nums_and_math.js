d93c4d39389e7e7808f2bfaa2d4c6a7cdab13acc
puneetuttam <puneet.uttam.99@gmail.com>
2023-12-12 19:33:00 +0530

    basic 1

---
+++ 01_basic/06_nums_and_math.js
@@ -0,0 +1,37 @@
+const score=400
+console.log(score)
+
+const balance =new Number(100)
+console.log(balance);
+ 
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber =23.3434
+console.log(otherNumber.toPrecision(3));
+
+const num=100000000;
+console.log(num.toLocaleString());
+
+//+++++++++++++++++++++ Math ++++++++++++++++
+
+
+console.log(Math);
+console.log(Math.abs(-4));
+
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.6));
+console.log(Math.floor(4.6));
+
+console.log(Math.min(1,2,3,4,5,6));
+console.log(Math.max(1,2,3,4,5,6));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
+
+const min=10;
+const max=20;
+
+console.log(Math.random() *(max-min +1)+min);
+
