1f56ceb7d7995ee7c714b9e10a8621d6932eeb42
puneetuttam <punnet.uttam.99@gmail.com>
2024-03-02 16:15:16 +0530

    Conversion Operation

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score="33";
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber =Number(score);
 
-console.log(typeof(valueInNumber));
+// console.log(typeof(valueInNumber));
 
 /**
  "33"=> 33
@@ -15,7 +15,7 @@ console.log(typeof(valueInNumber));
 
  let isLoggedIn="Puneet";
 let BooleanIsLoggedIn= Boolean(isLoggedIn);
-console.log(BooleanIsLoggedIn);
+// console.log(BooleanIsLoggedIn);
 
 //1=> true; 0=> false;
 //""=> false;
@@ -23,7 +23,44 @@ console.log(BooleanIsLoggedIn);
 
 let someNumber=33;
 let StringNumber =String(someNumber)
-console.log(typeof StringNumber);
+// console.log(typeof StringNumber);
 
 
+//******************Operation************* */
+
+let value=3;
+let negativevalue=-value;
+
+console.log(negativevalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(3%2);
+
+let str1="Hello, "
+let str2="Puneet";
+let str3=str1+str2;
+console.log(str3);
+
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+console.log(+true);
+// console.log(true+); //error
+console.log(+"");
+
+let num1,num2,num3;
+num1=num2=num3=3;
+console.log(num3);
+
+let gameCounter=100;
+gameCounter++;
+++gameCounter;
+console.log(gameCounter);
 
