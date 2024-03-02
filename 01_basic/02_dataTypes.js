b7f2ae75b255268e9a151b47a624589ce4d4d28a
puneetuttam <punnet.uttam.99@gmail.com>
2024-03-02 14:31:58 +0530

    Data Types

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict";
+//alert(3+3); // we are using node.js, not browser. This will only work on browser
+
+
+console.log("Puneet")
+let name="Puneet";
+let age=18;
+let isLoggedIn=false;
+
+
+//number => 2 to power 52;
+// bigint
+// String = "" or ''
+//boolean => true/false
+//null => standalone value
+//undefined=>  
+//Symbol=> unique
+
+console.log(typeof null); //objects
+console.log(typeof undefine); //not defined
+ 
