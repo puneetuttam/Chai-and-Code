6a60b29b513fd610a23d4a604236108ff550463a
puneetuttam <punnet.uttam.99@gmail.com>
2024-03-02 15:20:18 +0530

    Conversion Operation

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score="33";
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber =Number(score);
+
+console.log(typeof(valueInNumber));
+
+/**
+ "33"=> 33
+ "33abc"=> NaN
+ true=1; false=0;
+ */
+
+ let isLoggedIn="Puneet";
+let BooleanIsLoggedIn= Boolean(isLoggedIn);
+console.log(BooleanIsLoggedIn);
+
+//1=> true; 0=> false;
+//""=> false;
+// "Puneet"=> true;
+
+let someNumber=33;
+let StringNumber =String(someNumber)
+console.log(typeof StringNumber);
+
+
+
