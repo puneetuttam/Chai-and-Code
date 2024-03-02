a8b67fd9425328b1e6a22738f0e0cc2d979f2742
puneetuttam <punnet.uttam.99@gmail.com>
2024-03-02 16:23:05 +0530

    Comparison

---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,13 @@
+// console.log("2">1);;
+// console.log("02">1);
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined<0);
+
+//=== It checks value strickly that means value and its data types
+console.log("2"===2);
