b83d6d2425ba651d803024f050d8d2920d8200d4
puneetuttam <punnet.uttam.99@gmail.com>
2024-03-03 20:53:37 +0530

    Number and Math

--- 01_basic/06_number_math.js
+++ 01_basic/06_number_math.js
@@ -33,7 +33,4 @@ console.log(Math.random()*10+1); //0 result will be handled like this=>if you do
 const min=10;
 const max=20;
 
-
 console.log(Math.floor(Math.random()*(max-min+1))+min);//this will give you result between  10 and 20
-
-
