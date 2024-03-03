691261b065ecb54e602ce4a365df441882e61a18
puneetuttam <punnet.uttam.99@gmail.com>
2024-03-04 00:31:57 +0530

    Dates

---
+++ 01_basic/07_Dates.js
@@ -0,0 +1,14 @@
+let myDate=new Date();
+
+// console.log(myDate);
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toString());
+
+let myCreatedDate=new Date(2023,0,23)
+//console.log(myCreatedDate.toDateString());
+
+let myTampStamp=Date.now();
+console.log(myCreatedDate.getTime());
+console.log(myDate.getMonth()+1);
+console.log(myDate.getFullYear());
