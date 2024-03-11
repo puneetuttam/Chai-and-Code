553f459ac559daf66d64b8add149659d69909709
puneetuttam <punnet.uttam.99@gmail.com>
2024-03-11 10:30:49 +0530

    02 Scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,10 +6,45 @@ if(true){
     let a=10
     const b=23
     var c=233
-    console.log("Innter", a);
+    //console.log("Innter", a);
 }
 
 //console.log(a);
 //console.log(b);
-console.log(a);
+//console.log(a);
+
+function one(){
+    const userName="Sam"
+    function two(){
+        const website="Youtube"
+        //console.log(userName);
+    }
+    //console.log(website);
+
+    two();
+}
+one();
+
+if(true){
+    const username= "Hitesh"
+    if(username == "Hitesh"){
+        const website="youtube"
+        //console.log(username+website);
+    }
+    //console.log(website);
+}
+//console.log(userName);
+
+//++++++++++++++++++++++ Inserting ++++++++++++++++
+
+function addone(num){
+    return num+1;
+}
+console.log(addone(4));
+
+//addTwo(123);
+const addTwo=function(num){
+    return num+2;
+}
+
 
