7e75c42f3c0fcce8769caf0e6f73c36e9f1a1dac
puneetuttam <punnet.uttam.99@gmail.com>
2024-03-11 10:10:02 +0530

    02 Scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,15 @@
+ let a=10
+// const b=23
+ var c=2332344
+
+if(true){
+    let a=10
+    const b=23
+    var c=233
+    console.log("Innter", a);
+}
+
+//console.log(a);
+//console.log(b);
+console.log(a);
+
