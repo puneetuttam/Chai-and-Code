938579ce59862ff6e045b7e4f5054b151c8acc43
puneetuttam <44168950+puneetuttam@users.noreply.github.com>
2024-03-20 09:36:19 +0000

    five

---
+++ 05_iteration/five.js
@@ -0,0 +1,39 @@
+const coding =["js","ruby","java","Python","cpp"]
+
+// coding.forEach( function (item){
+//     console.log(item);
+// })
+
+// coding.forEach((item)=>{
+//     console.log(item);
+// })
+
+
+function printMe(item){
+    console.log(item);
+}
+
+// coding.forEach(printMe)
+
+// coding.forEach((item,index,arr)=>{
+//     console.log(item,index,arr);
+// })
+
+const myCoding=[
+    {
+        languageName:"Javascript",
+        languageFileName:"js"
+    },
+    {
+        languageName:"python",
+        languageFileName:"py"
+    },
+    {
+        languageName:"java",
+        languageFileName:"java"
+    }
+]
+
+myCoding.forEach((item)=>{
+    console.log(item.languageName);
+})
